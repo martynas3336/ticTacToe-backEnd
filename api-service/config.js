38a1ac2d68e4cf95e19c24cb14fed281019667bd
Dealer <dealer@ns540195.ip-139-99-130.net>
2020-04-27 09:38:43 +0000

    .gitignore

---
+++ api-service/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  db: {
+    client:"mysql",
+    connection: {
+      port:3306,
+      host:"db",
+      user:"root",
+      password:"password",
+      database:"ticTacToe"
+    }
+  }
+}
